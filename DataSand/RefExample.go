25de41db0cbdba2babd5ea259129d915afff4415
Sharon Aicler <saichler@gmail.com>
2017-11-17 16:19:05 -0800

    Reflection example
    Signed-off-by: Sharon Aicler <saichler@gmail.com>

---
+++ DataSand/RefExample.go
@@ -0,0 +1,61 @@
+package main
+
+import ("reflect"
+        "./network"
+	"fmt"
+	"strings"
+)
+
+func main(){
+	structInstance := &network.Packet{}
+	sql := getCreateSqlStatementForStruct(structInstance,"dbname")
+	fmt.Println(sql)
+}
+
+func getCreateSqlStatementForStruct(any interface{}, dbName string) string {
+	v := reflect.ValueOf(any).Elem()
+
+	typeName := v.Type().Name()
+	sql := "CREATE TABLE IF NOT EXISTS "+dbName+"."+typeName +" (\n"
+	sql += addFields(reflect.TypeOf(any), typeName)
+	sql+= ");\n"
+
+	return sql
+}
+
+func addFields(interfaceType reflect.Type, typeName string) string {
+
+	sql := ""
+	typeName = "  "+typeName
+	if interfaceType.Kind()==reflect.Ptr {
+		interfaceType = interfaceType.Elem()
+	}
+
+	for fieldIndex := 0; fieldIndex<interfaceType.NumField(); fieldIndex++ {
+
+		field := interfaceType.Field(fieldIndex)
+
+		fieldName := field.Name
+		fieldKind := field.Type.Kind()
+
+		if field.Type.Kind()==reflect.Ptr {
+			fieldKind = field.Type.Elem().Kind()
+		}
+
+		switch fieldKind {
+		case reflect.Bool:
+			sql += typeName + fieldName + " bool ,\n"
+		case reflect.Uint32:
+			sql += typeName + fieldName + " integer ,\n"
+		case reflect.Struct:
+			sql += addFields(field.Type, fieldName+"_")
+		case reflect.String:
+			size := "128"
+			if strings.Contains(strings.ToLower(fieldName), "uuid") {
+				size = "64"
+			}
+			sql += typeName + fieldName + " VARCHAR[" + size + "] ,\n"
+		}
+	}
+	return sql
+}
